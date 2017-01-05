5ec298c3d8285b5d801c48793c79cb04b77df23d
harel9123 <harelkick@gmail.com>
2017-01-05 18:42:45 +0200

    .

--- screenShare.py
+++ screenShare.py
@@ -1,52 +1,18 @@
 from PIL import Image, ImageGrab
 import socket
-import struct
 from cStringIO import StringIO
 from time import sleep
-from base64 import b64encode
+import base64
 import win32api
 
 IP = '0.0.0.0'
 PORT = 8888
 
-def run():
-	s = socket.socket()
-	s.bind( ( IP , PORT ) )
-	s.listen(1)
-	con, addr = s.accept()
+s = socket.socket()
+s.bind( ( IP , PORT ) )
+s.listen(1)
+con, addr = s.accept()
 
-<<<<<<< HEAD
-	while True:
-		data = StringIO()
-		img = ImageGrab.grab()
-		img.save(data, 'png')
-		data = b64encode(data.getvalue())
-		l = len(data)
-		sent = 0
-		max_size = 65535
-		approval = con.recv(2)
-		if approval != "go":
-			continue
-
-		data = '(' + data + ')'
-		con.send(data)
-
-		ack = con.recv(15)
-		#print ack
-		if ack[:2] == 'ok':
-			ack = ack[2:]
-			ack = ack.split(',')
-			ack[0] = ack[0].strip('(')
-			ack[1] = ack[1].strip(')')
-			x = int(ack[0])
-			y = int(ack[1])
-			win32api.SetCursorPos( ( x, y ) )
-			continue
-
-	con.close()
-
-run()
-=======
 while True:
 	data = StringIO()
 	img = ImageGrab.grab()
@@ -55,10 +21,11 @@ while True:
 	l = len(data)
 	sent = 0
 	max_size = 65535
-	approval = s.recv(2)
+	approval = con.recv(2)
 	if approval != "go":
 		continue
-	for x in range( ( l / max_size ) + 1 ):
+
+	'''for x in range( ( l / max_size ) + 1 ):
 		sent += max_size
 		toSend = data[ : max_size]
 		#print len(toSend), x
@@ -74,7 +41,10 @@ while True:
 			break
 		#print ack #For delay
 		t = con.send( toSend )
-		#print t, x
+		#print t, x'''
+	data = '(' + data + ')'
+	con.send(data)
+
 	ack = con.recv(15)
 	#print ack
 	if ack[:2] == 'ok':
@@ -87,12 +57,4 @@ while True:
 		win32api.SetCursorPos( ( x, y ) )
 		continue
 
-	'''ack = con.recv(7)
-	if ack == 'success':
-		#sleep(5)
-		continue
-	else:
-		break'''
-
 con.close()
->>>>>>> parent of 5ba576c... Communication improvements
--- viewScreenPyQt.py
+++ viewScreenPyQt.py
@@ -1,61 +1,40 @@
-from PIL import Image
 import socket
-import struct
 import base64
-from threading import Thread
 import win32api
-from cStringIO import StringIO
 from time import sleep
 from PyQt4 import QtGui
 from PyQt4 import QtCore
-import sys, os
+from sys import argv, exit
+from os import getcwd
 
-#IP = '10.0.0.1'
-#IP = '127.0.0.1'
+IP = '10.0.0.1'
+IP = '127.0.0.1'
 PORT = 8888
 
 s = socket.socket()
+s.connect( ( IP , PORT ) )
 
-<<<<<<< HEAD
 app = QtGui.QApplication(argv)
-
-width = win32api.GetSystemMetrics(0)
-height = win32api.GetSystemMetrics(1)
-=======
-app = QtGui.QApplication(sys.argv)
->>>>>>> parent of 5ba576c... Communication improvements
 window = QtGui.QMainWindow()
-window.setGeometry(30, 30, width - 1, height - 1)
+window.setGeometry(30, 30, 1920, 1080)
+
+counter = 0
+
 pic = QtGui.QLabel(window)
-pic.setGeometry(0, 0, width - 1, height - 1)
+pic.setGeometry(0, 0, 1920, 1080)
 
-def run(IP):
-	
-	s.connect( ( IP , PORT ) )
-	window.show()
-
-	#while True:
-	#	foo(pic)
-	timer = QtCore.QTimer()
-	timer.timeout.connect(foo)
-	timer.start(0)
-
-	s.close()
-	
-	exit(app.exec_())
-
-
+window.show()
 
 def foo():
-	max_size = 65535
 	data = ''
-	temp = 'a' * max_size
 	temp = ''
 	c = 0
 	s.send('go')
-	while ')' is not in temp:
-		data += s.recv()
-		print len(data), c
+	sizeToRec = 65535
+	while ')' not in temp:
+		temp = s.recv(sizeToRec)
+		data += temp
+		#print len(data), c
 		#data += temp
 		c += 1
 	data = data[1:]
@@ -72,19 +51,10 @@ def foo():
 	p.write(data)
 	p.close()
 
-	pic.setPixmap(QtGui.QPixmap(os.getcwd() + '/p.png'))
-	#counter += 1
-	#s.send('success')
+	pic.setPixmap(QtGui.QPixmap(getcwd() + '/p.png'))
 
-#thread = Thread(target = foo)
-#thread.start()
-
-<<<<<<< HEAD
-run('127.0.0.1')
-=======
 timer = QtCore.QTimer()
 timer.timeout.connect(foo)
 timer.start(0)
 
-sys.exit(app.exec_())
->>>>>>> parent of 5ba576c... Communication improvements
+exit(app.exec_())
