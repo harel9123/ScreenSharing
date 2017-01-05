7d83a420efba617b9d7faca82f605de0b57a4600
harel9123 <harelkick@gmail.com>
2017-01-05 18:38:30 +0200

    .

--- screenShare.py
+++ screenShare.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageGrab
 import socket
+import struct
 from cStringIO import StringIO
 from time import sleep
 from base64 import b64encode
@@ -14,6 +15,7 @@ def run():
 	s.listen(1)
 	con, addr = s.accept()
 
+<<<<<<< HEAD
 	while True:
 		data = StringIO()
 		img = ImageGrab.grab()
@@ -44,3 +46,53 @@ def run():
 	con.close()
 
 run()
+=======
+while True:
+	data = StringIO()
+	img = ImageGrab.grab()
+	img.save(data, 'png')
+	data = base64.b64encode(data.getvalue())
+	l = len(data)
+	sent = 0
+	max_size = 65535
+	approval = s.recv(2)
+	if approval != "go":
+		continue
+	for x in range( ( l / max_size ) + 1 ):
+		sent += max_size
+		toSend = data[ : max_size]
+		#print len(toSend), x
+		data = data[max_size : ]
+		l = len(toSend)
+		l = str(l)
+		#print l
+		if len(l) < 5:
+			l = '0' + l
+		con.send(l)
+		ack = con.recv(2)
+		if ack != 'go':
+			break
+		#print ack #For delay
+		t = con.send( toSend )
+		#print t, x
+	ack = con.recv(15)
+	#print ack
+	if ack[:2] == 'ok':
+		ack = ack[2:]
+		ack = ack.split(',')
+		ack[0] = ack[0].strip('(')
+		ack[1] = ack[1].strip(')')
+		x = int(ack[0])
+		y = int(ack[1])
+		win32api.SetCursorPos( ( x, y ) )
+		continue
+
+	'''ack = con.recv(7)
+	if ack == 'success':
+		#sleep(5)
+		continue
+	else:
+		break'''
+
+con.close()
+>>>>>>> parent of 5ba576c... Communication improvements
--- viewScreenPyQt.py
+++ viewScreenPyQt.py
@@ -1,11 +1,14 @@
+from PIL import Image
 import socket
+import struct
 import base64
+from threading import Thread
 import win32api
+from cStringIO import StringIO
 from time import sleep
 from PyQt4 import QtGui
 from PyQt4 import QtCore
-from sys import argv, exit
-from os import getcwd
+import sys, os
 
 #IP = '10.0.0.1'
 #IP = '127.0.0.1'
@@ -13,10 +16,14 @@ PORT = 8888
 
 s = socket.socket()
 
+<<<<<<< HEAD
 app = QtGui.QApplication(argv)
 
 width = win32api.GetSystemMetrics(0)
 height = win32api.GetSystemMetrics(1)
+=======
+app = QtGui.QApplication(sys.argv)
+>>>>>>> parent of 5ba576c... Communication improvements
 window = QtGui.QMainWindow()
 window.setGeometry(30, 30, width - 1, height - 1)
 pic = QtGui.QLabel(window)
@@ -40,15 +47,15 @@ def run(IP):
 
 
 def foo():
+	max_size = 65535
 	data = ''
+	temp = 'a' * max_size
 	temp = ''
 	c = 0
 	s.send('go')
-	sizeToRec = 65535
-	while ')' not in temp:
-		temp = s.recv(sizeToRec)
-		data += temp
-		#print len(data), c
+	while ')' is not in temp:
+		data += s.recv()
+		print len(data), c
 		#data += temp
 		c += 1
 	data = data[1:]
@@ -65,6 +72,19 @@ def foo():
 	p.write(data)
 	p.close()
 
-	pic.setPixmap(QtGui.QPixmap(getcwd() + '/p.png'))
+	pic.setPixmap(QtGui.QPixmap(os.getcwd() + '/p.png'))
+	#counter += 1
+	#s.send('success')
 
+#thread = Thread(target = foo)
+#thread.start()
+
+<<<<<<< HEAD
 run('127.0.0.1')
+=======
+timer = QtCore.QTimer()
+timer.timeout.connect(foo)
+timer.start(0)
+
+sys.exit(app.exec_())
+>>>>>>> parent of 5ba576c... Communication improvements
