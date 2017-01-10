38e221d0eac214635ee3d501036f88f4e659c191
harel9123 <harelkick@gmail.com>
2017-01-10 09:45:31 +0200

    minor changes

--- Tests/events.py
+++ Tests/events.py
@@ -1,13 +1,37 @@
 import pythoncom, pyHook
+from time import sleep
+import Queue
+
+q = Queue()
+moved = False
+isClicked = (0, 0, 0)
+MOUSE_DOWN = 1
+MOUSE_UP = 2
+
+LEFT = 1
+RIGHT = 2
 
 def OnMouseEvent(event):
-    print 'MessageName:',event.MessageName
-    print 'Message:',event.Message
-    print 'Position:',event.Position
+	global isClicked
+	global q
+    print 'MessageName:', event.MessageName
+    print 'Message:', event.Message
+    if event.Message == "mouse left down":
+    	isClicked = (MOUSE_DOWN, LEFT, event.Position)
+    	q.put(isClicked)
+    elif event.Message == "mouse left up":
+    	isClicked = (MOUSE_UP, LEFT, event.Position)
+    	q.put(isClicked)
+    print 'Position:', event.Position
     print '---'
     return True
 
-hm = pyHook.HookManager()# create a hook manager
-hm.MouseAll = OnMouseEvent
-hm.HookMouse()
-pythoncom.PumpMessages()
+def pyHookHandle():
+	hm = pyHook.HookManager()# create a hook manager
+	#hm.KeyDown = OnKeyboardEvent# watch for all key events
+	hm.MouseAll = OnMouseEvent
+	#hm.HookKeyboard()# set the hook
+	hm.HookMouse()
+	pythoncom.PumpMessages()# wait forever
+
+pyHookHandle()
--- viewScreenPyQt.py
+++ viewScreenPyQt.py
@@ -6,9 +6,43 @@ from PyQt4 import QtGui
 from PyQt4 import QtCore
 from sys import argv, exit
 from os import getcwd
+import pythoncom, pyHook
+import Queue
+
+# q = Queue()
+moved = False
+isClicked = (0, 0, 0)
+MOUSE_DOWN = 1
+MOUSE_UP = 2
+
+LEFT = 1
+RIGHT = 2
+
+# def OnMouseEvent(event):
+# 	global isClicked
+# 	global q
+#     print 'MessageName:', event.MessageName
+#     print 'Message:', event.Message
+#     if event.Message == "mouse left down":
+#     	isClicked = (MOUSE_DOWN, LEFT, event.Position)
+#     	q.put(isClicked)
+#     elif event.Message == "mouse left up":
+#     	isClicked = (MOUSE_UP, LEFT, event.Position)
+#     	q.put(isClicked)
+#     print 'Position:', event.Position
+#     print '---'
+#     return True
+
+# def pyHookHandle():
+# 	hm = pyHook.HookManager()# create a hook manager
+# 	#hm.KeyDown = OnKeyboardEvent# watch for all key events
+# 	hm.MouseAll = OnMouseEvent
+# 	#hm.HookKeyboard()# set the hook
+# 	hm.HookMouse()
+# 	pythoncom.PumpMessages()# wait forever
 
 IP = '10.20.170.115'
-#IP = '127.0.0.1'
+IP = '127.0.0.1'
 PORT = 8888
 
 s = socket.socket()
